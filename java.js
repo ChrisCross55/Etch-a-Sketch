83a588eb749a3449a6ebd49d72d6c6c422f261b8
ChrisCross55 <114107673+ChrisCross55@users.noreply.github.com>
2022-11-21 23:01:04 -0800

    add button to change canvas size

--- java.js
+++ java.js
@@ -1,15 +1,29 @@
-function createDivs(id,amount) {
-    const container=document.getElementById(id);
+
+function createDivs() {
+    
+
+    let blocks= prompt('Please enter number of blocks to be length and width', '16');
+    if (blocks>100) { blocks=100};
+    let amount= blocks*blocks;
+    let side= (960/blocks);
+    const container=document.getElementById('container');
+    container.innerHTML = '';               
     for ( let i =0; i < amount ; i++) { 
         const div= document.createElement('div');
         div.classList.add('pad');
+        div.style.width= `${side}px`;
+        div.style.height= `${side}px`;
         container.appendChild(div);
     }
+    etchSketch()
+
+
 }
 
-createDivs('container',256);
+const button= document.getElementById('size');
+button.addEventListener('click',createDivs);
 
-document.querySelectorAll('.pad').forEach (
+function etchSketch() {document.querySelectorAll('.pad').forEach (
 item =>{item.addEventListener('mouseover',()=> {
     item.classList.add('filled');
-})});
+})});}
